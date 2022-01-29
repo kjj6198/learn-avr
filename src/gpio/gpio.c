29fbcb0c52a003005cae67a7597f12534e3884f1
kjj6198 <kjj6198@gmail.com>
2022-01-29 22:06:18 +0900

    update avr code

--- src/gpio/gpio.c
+++ src/gpio/gpio.c
@@ -1,14 +1,20 @@
 #define F_CPU 1000000L
 #include <avr/io.h>
 #include <util/delay.h>
-#ifdef DEBUG
-#include <avr/include/avr/iom328p.h>
-#endif
 
 int main(void)
 {
-  DDRB = (1 << DDB0);     // set PORTB0 as output and port1 ~ 7 as ouput. DDR means data direction register
-  PORTB = (1 << PORTB0);  // set PORTB0 to 1 (HIGH)
-  unsigned char i = PINB; // read pin from PINB register
+  while (1)
+  {
+    DDRB = (1 << DDB0);     // set PORTB0 as output and port1 ~ 7 as ouput. DDR means data direction register
+    PORTB = (1 << PORTB0);  // set PORTB0 to 1 (HIGH)
+    unsigned char i = PINB; // read pin from PINB register
+    _delay_ms(500);
+    PORTB = ~(1 << PORTB0);
+    _delay_ms(500);
+    bit(PORTB);
+  }
   return 0;
 }
+
+
